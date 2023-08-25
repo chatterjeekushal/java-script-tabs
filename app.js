7232fbfd89acd24a2320ebd3b0ac3a8f69c2f7b1
kushal <kushalchatterjee943@gmail.com>
2023-08-25 16:31:01 +0530

    final submit

---
+++ app.js
@@ -0,0 +1,28 @@
+let btngroup=document.querySelectorAll(".btnlink");
+ 
+ 
+ for(let i=0; i<btngroup.length;i++){
+
+btngroup[i].addEventListener('click',function(){
+
+
+    let tabname=this.dataset.tab;
+    let tabcontent=document.getElementById(tabname);
+
+    let tabcontentmain=document.querySelectorAll(".tabcontent");
+
+    for(let k=0; k <tabcontentmain.length; k++){
+
+
+        tabcontentmain[k].style.display="none";
+    }
+
+
+    tabcontent.style.display="block";
+
+    
+
+})
+
+ }
+
